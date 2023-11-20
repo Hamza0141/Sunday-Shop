c37570881e8ddf6dee148b4848d5c6e4fed3fa93
Hamza0141 <131574624+Hamza0141@users.noreply.github.com>
2023-11-20 04:05:46 -0600

    User Setup

--- app.js
+++ app.js
@@ -10,10 +10,7 @@ app.use(express.json());
 const userRoute = require("./Api/Routes/use.route")
 
 app.use(userRoute);
-app.get("/test",(req , res )=>{
-res.end("its working ")
 
-})
 app.listen(port,()=>{
     console.log(`app listing on ${port}`)
 
